16ec6b7314560c9f1bcf931bd0492668e271527d
Arlen <nasirov.munar1976@gmail.com>
2026-06-10 17:40:28 +0600

    final result

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
 var name = "Alex"
 
-var age = 25
+var age = 20
+
+var isStudent = true
+
+var project = null
+
+var job
+
+console.log(name);
+console.log(age);
+console.log(isStudent);
+console.log(project);
+console.log(job);
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isStudent);
+console.log(typeof project);
+console.log(typeof job); 
+
+var person = {
+    name: "Alex",
+    age: 20,
+    isStudent: true
+};
+
+console.log("Меня зовут " + person.name + ", мне " + person.age + " лет.");
+
+var favoriteMovies = [
+    "Джанго освобожденный",
+    "Однажды в голливуде",
+    "Зеленая книга",
+    "Первому игроку приготовиться",
+    "Властелин колец"
+]; 
